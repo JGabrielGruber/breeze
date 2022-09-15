cb12813e2fba5fce2474aa97899281e01de38ff6
José Gabriel Gruber <jgabrielgruber@hotmail.com>
2022-09-15 14:36:55 -0300

    Added branging assets

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,18 +1,32 @@
+import { Head } from "https://deno.land/x/fresh@1.1.1/runtime.ts";
+
 import Counter from "../islands/Counter.tsx";
 
 export default function Home() {
   return (
-    <div class="p-4 mx-auto max-w-screen-md">
-      <img
-        src="/logo.svg"
-        class="w-32 h-32"
-        alt="the fresh logo: a sliced lemon dripping with juice"
-      />
-      <p class="my-6">
-        Welcome to `fresh`. Try updating this message in the ./routes/index.tsx
-        file, and refresh.
-      </p>
-      <Counter start={3} />
-    </div>
+    <>
+      <Head>
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+        <meta name="msapplication-TileColor" content="#00aba9" />
+        <meta name="theme-color" content="#ffffff" />
+        <title>Breeze</title>
+      </Head>
+      <div class="p-4 mx-auto max-w-screen-md">
+        <img
+          src="/logo.svg"
+          class="w-32 h-32"
+          alt="the fresh logo: a sliced lemon dripping with juice"
+        />
+        <p class="my-6">
+          Welcome to `fresh`. Try updating this message in the ./routes/index.tsx
+          file, and refresh.
+        </p>
+        <Counter start={3} />
+      </div>
+    </>
   );
 }
