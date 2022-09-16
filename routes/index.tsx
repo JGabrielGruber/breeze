0fda7bf59bf81d437aae914356c4b492653a8f47
José Gabriel Gruber <jgabrielgruber@hotmail.com>
2022-09-16 08:12:16 -0300

    Implemented PWA

--- routes/index.tsx
+++ routes/index.tsx
@@ -27,6 +27,11 @@ export default function Home() {
         </p>
         <Counter start={3} />
       </div>
+      <script type="module">
+        import 'https://cdn.jsdelivr.net/npm/@pwabuilder/pwaupdate/dist/pwa-update.js';
+        const el = document.createElement('pwa-update');
+        document.body.appendChild(el);
+      </script>
     </>
   );
 }
